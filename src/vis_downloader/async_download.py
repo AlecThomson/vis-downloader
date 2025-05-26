dc27f0829a53f8d0194ca46ba52d10a5a19b3c0a
tgalvin <tim.galvin@csiro.au>
2025-05-26 17:56:25 +1000

    silly

--- src/vis_downloader/async_download.py
+++ src/vis_downloader/async_download.py
@@ -314,7 +314,7 @@ async def get_cutouts_from_casda(
         coros = [stage_and_download(
                     sbid=sbid, result_row=row, output_dir=download_options.output_dir, casda=casda
                 ) for row in result_table]
-        for path in gather_with_limit(coros, as_completed=True):
+        async for path in gather_with_limit(coros, as_completed=True):
             async with inner_semaphore:
                 if download_options.extract_tar:
                     path = await asyncio.to_thread(extract_tarball(in_path=path))
